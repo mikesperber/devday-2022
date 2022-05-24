7dc7ef5182560426be075504850689f300a4437c
Mike Sperber <sperber@deinprogramm.de>
2022-05-24 11:00:44 +0200

    Update

--- haskell-code/ContractMike.hs
+++ haskell-code/ContractMike.hs
@@ -8,7 +8,11 @@ Zero Coupon Bond / Zero-Bond
 "Ich bekomme am 24.12.2022 100€."
 
 Schritt 2:
-einfaches Beispiel in kleinere Bestandteile zerlegen
+einfaches Beispiel in kleinere Bestandteile/Ideen zerlegen
+- Währung "1€ jetzt"
+- Betrag/Vielfaches
+- Später
+
 -}
 data Currency = EUR | GBP
   deriving Show 
